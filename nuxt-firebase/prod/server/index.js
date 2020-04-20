3a4ec96e596bf220f81881e9e2a5e33b14175b6b
JrTRD <ayoubtrdbuzzer@gmail.com>
2020-04-20 22:00:32 +0000

    initial commit

---
+++ nuxt-firebase/prod/server/index.js
@@ -0,0 +1,30 @@
+const functions = require("firebase-functions");
+const { Nuxt } = require("nuxt");
+const express = require("express");
+const app = express();
+
+const config = {
+  dev: false,
+  buildDir: "nuxt",
+  build: {
+    publicPath: "/assets/"
+  }
+};
+const nuxt = new Nuxt(config);
+
+function handleRequest(req, res) {
+  console.log("log3");
+  res.set("Cache-Control", "public, max-age=300, s-maxage=600");
+  return new Promise((resolve, reject) => {
+    nuxt.render(req, res, promise => {
+      promise.then(resolve).catch(reject);
+    });
+  });
+}
+
+
+app.use(handleRequest);
+
+const server = require("./server")
+exports.nuxtssr = functions.https.onRequest(app);
+exports.server = functions.https.onRequest(server)
